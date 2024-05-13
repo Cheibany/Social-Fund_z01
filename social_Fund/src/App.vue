0cfde528cdb98e44fcbc2b88ca4a73571b702dbe
Cheibany <ciresall79@gmail.com>
2024-05-13 19:53:44 +0000

    Update-addLogin

---
+++ social_Fund/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <login-modal></login-modal>
+  </div>
+</template>
+
+<script>
+import LoginModal from './components/Login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginModal
+  }
+}
+</script>yy./components/Login.vue
---
+++ social_Fund/src/assets/js/LoginModal.js
@@ -0,0 +1,42 @@
+export default {
+  data() {
+    return {
+      isOpened: false
+    };
+  },
+  methods: {
+    openModal() {
+      const modal = document.querySelector(".modal");
+      const body = document.querySelector("body");
+      modal.classList.add("is-open");
+      body.style.overflow = "hidden";
+    },
+    closeModal() {
+      const modal = document.querySelector(".modal");
+      const body = document.querySelector("body");
+      modal.classList.remove("is-open");
+      body.style.overflow = "initial";
+    }
+  },
+  mounted() {
+    const modalButton = document.querySelector(".modal-button");
+    const closeButton = document.querySelector(".close-button");
+    const scrollDown = document.querySelector(".scroll-down");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > window.innerHeight / 3 && !this.isOpened) {
+        this.isOpened = true;
+        scrollDown.style.display = "none";
+        this.openModal();
+      }
+    });
+
+    modalButton.addEventListener("click", this.openModal);
+    closeButton.addEventListener("click", this.closeModal);
+
+    document.onkeydown = evt => {
+      evt = evt || window.event;
+      if (evt.keyCode === 27) this.closeModal();
+    };
+  }
+}
